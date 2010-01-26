95fbd52b6b5cca2e722dec4ba98677d6d803530a
KDr2 <kdr2@x-macro.com>
2010-01-26 08:53:08 +0800

    modify config

--- source/conf.py
+++ source/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -103,10 +103,10 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "KDr2's Personal Website"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = "KDr2.net"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
