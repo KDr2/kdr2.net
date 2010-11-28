65c92b0de743c1ae3a13a67fceee6c4f8abc0988
KDr2 <kdr2@x-macro.com>
2010-11-28 23:14:22 +0800

    ...

--- kdr2net/kdr2net/controllers/comments.py
+++ kdr2net/kdr2net/controllers/comments.py
@@ -24,7 +24,7 @@ class CommentsController(BaseController):
     def get_comments(self,user_url=""):
         comments=Session.query(Comment).filter_by(target=user_url).order_by(Comment.date)
         comments_map=map(Comment.dict,comments)
-        return json.dumps(comments)
+        return json.dumps(comments_map)
 
     def post_comment(self):
         #if(request.method!="POST"):
