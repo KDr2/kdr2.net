ca18b93015125a7008a25381e7dc34c7319f1d34
KDr2 <killy.draw@gmail.com>
2011-06-28 11:57:19 +0800

    delete pylons source

--- kdr2net/kdr2net/tests/functional/test_comments.py
+++
@@ -1,7 +0,0 @@
-from kdr2net.tests import *
-
-class TestCommentsController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='comments', action='index'))
-        # Test response...
