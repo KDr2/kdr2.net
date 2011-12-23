98cf885cff749a299facc0358902db1c404666eb
KDr2 <killy.draw@gmail.com>
2011-12-23 20:34:59 +0800

    del ocaml code in master

--- service/config.ml
+++
@@ -1,7 +0,0 @@
-(*
- * file : config.ml
- * author : KDr2
- *)
-
-let changelog = "/Volumes/KDr2/Work/mine/kdr2.net/source/changelog.js";;
-let db_url = "/Volumes/KDr2/Work/mine/kdr2.net/data/kdr2.db";;
