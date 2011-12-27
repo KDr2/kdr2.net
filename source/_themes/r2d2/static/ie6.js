0b0ccd4c0c3156dde1e840cdb0ea4b0170f1c630
KDr2 <killy.draw@gmail.com>
2011-12-27 19:58:43 +0800

    giveup ie6

---
+++ source/_themes/r2d2/static/ie6.js
@@ -0,0 +1,13 @@
+/*
+ * author : KDr2 
+ */
+
+function goto_ie_site(){
+    var ie_home="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home"
+    window.location=ie_home
+}
+
+alert("This Site will not support IE6 any longer, Please upgrade your OLD Browser!!!")
+setTimeout(goto_ie_site,3)
+
+
